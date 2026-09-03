4a64acf69203ef2123ae5e1db982a5eba27e60a3
lufseccus <congthanhvu1208@gmail.com>
2026-09-03 13:06:27 +0200

    Changed the xterm terminal name

--- kuka_udp_bridge_node/launch/dual_robot.launch.py
+++ kuka_udp_bridge_node/launch/dual_robot.launch.py
@@ -5,7 +5,7 @@ def generate_launch_description():
     return LaunchDescription([
         ExecuteProcess(
             cmd=[
-                'xterm', '-e', 'ros2', 'run', 'kuka_udp_bridge_node', 'udp_bridge_node',
+                'xterm', '-T', 'KMR 1', '-e', 'ros2', 'run', 'kuka_udp_bridge_node', 'udp_bridge_node',
                 '--ros-args', '-r', '__ns:=/robot1',
                 '-p', 'robot_ip:=172.31.1.10', '-p', 'client_port:=30333', '-p', 'network_interface:=eth0'
             ],
@@ -13,7 +13,7 @@ def generate_launch_description():
         ),
         ExecuteProcess(
             cmd=[
-                'xterm', '-e', 'ros2', 'run', 'kuka_udp_bridge_node', 'udp_bridge_node',
+                'xterm', '-T', 'KMR 2', '-e', 'ros2', 'run', 'kuka_udp_bridge_node', 'udp_bridge_node',
                 '--ros-args', '-r', '__ns:=/robot2',
                 '-p', 'robot_ip:=172.31.1.10', '-p', 'client_port:=30335', '-p', 'network_interface:=eth1'
             ],
